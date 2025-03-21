980ea729c554cdfba211101fe4888503370547ef
Aleksandr Omelucik <bladewizard@mail.ru>
2025-03-21 08:46:10 +0300

    decide scroll problem

--- burger.js
+++ burger.js
@@ -1,7 +1,9 @@
 const BURGER_OPENED_CLASSNAME = 'burger_open';
 const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
-const BODY_FIXED_CLASSNAME_2 = 'body_fixed';
+const BODY_FIXED_CLASSNAME_2 = 'body_fixed_2';
+const HTML_FIXED_CLASSNAME_2 = 'html_fixed';
 
+const htmlNode2 = document.querySelector("html")
 const bodyNode2 = document.querySelector('body');
 const burgerNode = document.querySelector('.js-burger');
 const burgerBtnNode = document.querySelector('.js-burger-btn');
@@ -21,4 +23,5 @@ function toggleBurger() {
     burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
     bodyNode2.classList.toggle(BODY_FIXED_CLASSNAME_2);
     burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
+    htmlNode2.classList.toggle(HTML_FIXED_CLASSNAME_2)
 }
--- popup.js
+++ popup.js
@@ -1,7 +1,9 @@
 const POPUP_OPENED_CLASSNAME = 'popup_open';
-const BODY_FIXED_CLASSNAME = "body__fixed";
+const BODY_FIXED_CLASSNAME = "body_fixed";
+const HTML_FIXED_CLASSNAME = "html_fixed"
 
 const bodyNode = document.querySelector("body");
+const htmlNode = document.querySelector("html")
 const popupNode = document.querySelector('.js-popup');
 const btnOpenNode = document.querySelector('.js-btn');
 const popupContentNode = document.querySelector(".js-popup__content")
@@ -21,4 +23,5 @@ popupNode.addEventListener("click" , (event) =>{
 function togglePopup() {
     popupNode.classList.toggle(POPUP_OPENED_CLASSNAME);
     bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
+    htmlNode.classList.toggle(HTML_FIXED_CLASSNAME)
 }
